2954f7d8d8da40d2907eed9d3d45b231041a34ac
Ira Russeva <ira_russeva@abv.bg>
2018-03-10 00:48:56 +0200

    Problem 3 documented

--- intro-and- basic-syntax/exercise/Problem 3/Problem 3/Problem3.cs
+++ intro-and- basic-syntax/exercise/Problem 3/Problem 3/Problem3.cs
@@ -4,8 +4,11 @@ class MilesToKilometers
 {
     static void Main(string[] args)
     {
+		// Receive distance in miles from Console
         double miles = double.Parse(Console.ReadLine());
+		// Convert miles to kilometers
         double kilometers = miles * 1.60934;
+		//TODO...
         Console.WriteLine($"{kilometers:f2}");
     }
 }
