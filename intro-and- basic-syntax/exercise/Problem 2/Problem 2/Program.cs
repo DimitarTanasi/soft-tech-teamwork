380c1eed4be34751c560023beb3d89ac6b7a2443
Ira Russeva <ira_russeva@abv.bg>
2018-03-09 10:09:25 +0200

    2 folders created(intro-and-basic-syntax, excercises). Problem 2 added

---
+++ intro-and- basic-syntax/exercise/Problem 2/Problem 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1_23jan18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double widht = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            double area = widht * height;
+
+            Console.WriteLine($"{area:f2}");
+        }
+    }
+}
