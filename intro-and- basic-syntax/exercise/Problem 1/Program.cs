7e0709d346eb086341bab198307d335ed0a76046
DimitarTanasi <mitko_107@abv.bg>
2018-03-14 17:04:49 +0200

    comitting steps d and e

--- intro-and- basic-syntax/exercise/Problem 1/Program.cs
+++ intro-and- basic-syntax/exercise/Problem 1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//Confirmed from Mu7k0
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,13 @@ namespace Debit_Card_Number
     {
         static void Main(string[] args)
         {
+            //We recieve from the Console parts of the debit card number (as integers)
+            //Mu7k0 cannot change the variable for this problem
             int firstPart = int.Parse(Console.ReadLine());
             int second = int.Parse(Console.ReadLine());
             int third = int.Parse(Console.ReadLine());
             int fourth = int.Parse(Console.ReadLine());
-
+            //Printing DebitCard number(as string)
             Console.WriteLine($"{firstPart:D4} {second:D4} {third:D4} {fourth:d4}");
 
         }
