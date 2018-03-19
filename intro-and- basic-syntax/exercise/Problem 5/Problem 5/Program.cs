13fa2bdafa9c27f4f7f2044e9ccc523c7e649333
Ira Russeva <ira_russeva@abv.bg>
2018-03-19 14:19:09 +0200

    Problem5 added

---
+++ intro-and- basic-syntax/exercise/Problem 5/Problem 5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Character_stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int currentHealth = int.Parse(Console.ReadLine());
+            int maximumHealth = int.Parse(Console.ReadLine());
+            int currentEnergy = int.Parse(Console.ReadLine());
+            int maximumEnergy = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}");
+
+            int countOfHealthDots = maximumHealth - currentHealth;
+
+            Console.WriteLine("Health: |{0}{1}|", new string('|', currentHealth), new string('.', countOfHealthDots));
+
+
+            int countOfPipes = currentEnergy;
+            int countOfdots = maximumEnergy - currentEnergy;
+
+            Console.WriteLine("Energy: |{0}{1}|", new string('|', countOfPipes), new string('.', countOfdots));
+        }
+    }
+}
