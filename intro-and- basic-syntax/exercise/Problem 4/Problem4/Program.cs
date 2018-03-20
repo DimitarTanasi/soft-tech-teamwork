ee063b6ddd8a06bf78cae64bd40c7d859dad53e4
margaritov <lubo@hush.com>
2018-03-20 09:56:40 +0200

    documented and confirmed code for Problem 4

--- intro-and- basic-syntax/exercise/Problem 4/Problem4/Program.cs
+++ intro-and- basic-syntax/exercise/Problem 4/Problem4/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//CONFIRMED from margaritov
+using System;
 
 namespace Problem4
 {
@@ -6,14 +7,18 @@ namespace Problem4
     {
         static void Main(string[] args)
         {
+			// We receive beverage name and volume from the console
             var BeverageName = Console.ReadLine();
             var BeverageVolume = int.Parse(Console.ReadLine());
+			//We receive energy and sugar data
             var energyContent = int.Parse(Console.ReadLine());
             var sugarContent = int.Parse(Console.ReadLine());
-
+			
+			// We calculate  energy and sugar contained in beverage
             var totalCalories = BeverageVolume * energyContent / 100.0;
             var totalSugar = BeverageVolume * sugarContent / 100.0;
 
+			// We print calculated data to the console 
             Console.WriteLine($"{BeverageVolume}ml {BeverageName}:");
             Console.WriteLine($"{totalCalories}kcal, {totalSugar}g sugars");
         }
